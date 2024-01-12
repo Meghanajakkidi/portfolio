fc923ef96c95e0139e4b21f10cdbc852b3adb192
meghana <meghanabscmpcs@gmail.com>
2024-01-12 16:45:31 +0530

    html code

---
+++ script.js
@@ -0,0 +1,10 @@
+let homediv = document.getElementById('homecontainer')
+let educationdiv = document.getElementById('educationcontainer')
+let skillsdiv = document.getElementById('skillscontainer')
+let projectsdiv = document.getElementById('projectcontainer')
+let contactdiv = document.getElementById('contactcontainer')
+homediv.style.display='none'
+educationdiv.style.display='none'
+skillsdiv.style.display='none'
+projectsdiv.style.display='none'
+contactdiv.style.display='none'
